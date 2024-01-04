dcc193b761d27dd55cf152bb343cf23e58313f51
kouwa stephane <kksasteph888@gmail.com>
2024-01-04 16:11:08 +0100

    Maj

--- src/HandlerClass/BlockContentAccess.php
+++ src/HandlerClass/BlockContentAccess.php
@@ -5,6 +5,8 @@ namespace Drupal\lesroidelareno\HandlerClass;
 use Drupal\block_content\BlockContentAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\lesroidelareno\lesroidelareno;
+use Drupal\Core\Access\AccessResult;
 
 class BlockContentAccess extends BlockContentAccessControlHandler {
   
@@ -18,6 +20,28 @@ class BlockContentAccess extends BlockContentAccessControlHandler {
    * @see \Drupal\blockscontent\BlocksContentsAccessControlHandler::checkAccess()
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    $isOwnerSite = lesroidelareno::isOwnerSite();
+    $IsAdministratorSite = lesroidelareno::userIsAdministratorSite();
+    $isAdministrator = lesroidelareno::isAdministrator();
+    switch ($operation) {
+      case 'view':
+        if ($entity->isPublished()) {
+          return AccessResult::allowed();
+        }
+        elseif ($isAdministrator)
+          return AccessResult::allowed();
+        break;
+      case 'update':
+      case 'delete':
+        if ($isAdministrator)
+          return AccessResult::allowed();
+        elseif ($isOwnerSite) {
+          return AccessResult::allowed();
+        }
+        elseif ($IsAdministratorSite) {
+          return AccessResult::allowed();
+        }
+    }
     return parent::checkAccess($entity, $operation, $account);
   }
   
