db5a78dde0ce1cbe23d29412115462aa92449900
kouwa stephane <kksasteph888@gmail.com>
2025-02-04 16:07:56 +0100

    Maj

--- src/HandlerClass/ListBuilderParagraph.php
+++ src/HandlerClass/ListBuilderParagraph.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Url;
+use Drupal\Core\Render\Markup;
 
 /**
  * Provides a custom list builder for MyEntity entities.
@@ -38,6 +39,13 @@ class ListBuilderParagraph extends EntityListBuilder {
   protected $current_path;
   
   /**
+   * The database connection to use.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+  
+  /**
    *
    * {@inheritdoc}
    */
@@ -46,6 +54,7 @@ class ListBuilderParagraph extends EntityListBuilder {
       'id' => $this->t('ID'),
       'label' => $this->t('Label'),
       'type' => 'Type',
+      'utilisation' => 'Utilisation',
       'field_domain_access' => 'Domain'
     ];
     return $header + parent::buildHeader();
@@ -69,9 +78,127 @@ class ListBuilderParagraph extends EntityListBuilder {
       'id' => $entity->id(),
       'label' => $entity->label(),
       'type' => $entity->bundle(),
+      'utilisation' => $this->countUtilisation($entity),
       'field_domain_access' => $domains
     ];
     return $row + parent::buildRow($entity);
+  }
+  
+  /**
+   * Compte le nombre de fois ou le paragraph est utilisé en function de
+   * certains type de contenu.
+   */
+  protected function countUtilisation(\Drupal\paragraphs\Entity\Paragraph $entity) {
+    $connection = $this->getDatabase();
+    $paragraph_id = $entity->id();
+    /**
+     * Liste des entites qui peuvent contenir des paragraphes.
+     *
+     * @var array $entitiesKeepParagraphs
+     */
+    $entitiesKeepParagraphs = [
+      'blocks_contents',
+      'node',
+      'taxonomy_term',
+      'user',
+      'commerce_product',
+      'commerce_product_variation',
+      'hbk_collection',
+      'site_internet_entity',
+      'site_type_datas',
+      'block_content'
+    ];
+    $links = [];
+    foreach ($entitiesKeepParagraphs as $entity_id) {
+      $fields = $this->getListManuelFieldReferenceParagraph($entity_id);
+      foreach ($fields as $field_name => $field) {
+        if ($field['is_mulple']) {
+          $field_parent_reference = 'entity_id';
+          $query = $connection->select($entity_id . '__' . $field['field_name'], 'f');
+          $query->fields('f', [
+            $field_parent_reference
+          ]);
+          $query->condition('f.' . $field_name . '_target_id', $paragraph_id);
+          $result = $query->execute()->fetchAssoc();
+        }
+        else {
+          $field_parent_reference = $field['field_name'];
+          $query = $connection->select($entity_id . '_field_data', 'f');
+          $query->fields('f', [
+            $field_parent_reference
+          ]);
+          $query->condition('f.' . $field_name, $paragraph_id);
+          $result = $query->execute()->fetchAssoc();
+        }
+        if ($result) {
+          $referenceEntity = \Drupal::entityTypeManager()->getStorage($entity_id)->load($result[$field_parent_reference]);
+          if ($referenceEntity)
+            $links[] = [
+              'title' => Markup::create($entity_id . ' | ' . $referenceEntity->id() . ' | ' . $referenceEntity->label()),
+              'url' => $referenceEntity->toUrl()
+            ];
+        }
+      }
+    }
+    if ($links) {
+      return [
+        'data' => [
+          '#theme' => 'links',
+          '#links' => $links
+        ]
+      ];
+    }
+    return '';
+  }
+  
+  /**
+   * Recupere les champs reference au paragraphes qui ont été creer
+   * manuellement.
+   */
+  protected function getListManuelFieldReferenceParagraph($entyty_id) {
+    $ids = [];
+    /**
+     *
+     * @var \Drupal\Core\Entity\EntityFieldManager $entity_field_manager
+     */
+    $entity_field_manager = \Drupal::service('entity_field.manager');
+    foreach ($entity_field_manager->getBaseFieldDefinitions($entyty_id) as $field_name => $field) {
+      /**
+       *
+       * @var \Drupal\Core\Field\BaseFieldDefinition $field
+       */
+      if (($field->getType() == 'entity_reference' || $field->getType() == 'entity_reference_revisions') && $field->getSetting('target_type') == 'paragraph') {
+        $ids[$field_name] = [
+          'is_mulple' => $field->isMultiple(),
+          'field_name' => $field->getName()
+        ];
+      }
+    }
+    return $ids;
+  }
+  
+  /**
+   * Recupere les champs reference au paragraphes qui ont été creer via
+   * l'interface utilisateur.
+   */
+  protected function getListCreateFieldReferenceParagraph() {
+    /**
+     *
+     * @var \Drupal\Core\Entity\Query\QueryInterface $query
+     */
+    $query = \Drupal::entityTypeManager()->getStorage('field_storage_config')->getQuery();
+    $query->condition('settings.target_type', 'paragraph');
+    return $query->execute();
+  }
+  
+  /**
+   * Recupere la connexion a la base de donnee.
+   */
+  protected function getDatabase() {
+    if (!$this->database) {
+      $this->database = \Drupal::database();
+    }
+    return $this->database;
   }
   
   /**
